d1450bbdf5811a3cbd5d07f80c14dbe80c90df44
BijelicLukas <Bijelic.Lukas.2003@gmail.com>
2025-03-07 23:14:03 +0100

    Oh Shit Commits
    Jo, ich hab jetzt die State Sache und Event gemacht. Es gibt jetzt 9 Buttons die ein Sound abspielen und den Fokus des Raumes wechselt. Nice oder. HOLY SHIT 381 changes. Ups!

---
+++ GameJam2025_LostSignal/Assets/Scripts/Gameplay/RoomManager.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+   public enum RoomState
+    {
+        None,
+        LT,
+        MT,
+        RT,
+        LM,
+        MM,
+        RM,
+        LL,
+        ML,
+        RL
+    }
+
+    private RoomState currentState;
+
+    private void Start()
+    {
+        SetRoomState(RoomState.None);
+    }
+
+    public event Action<RoomState> OnRoomStateChanged;
+
+    public void SetRoomState(RoomState newState)
+    {
+        currentState = newState;
+        Debug.Log($"We changed our focus to Room {currentState}");
+        OnRoomStateChanged?.Invoke(currentState);
+    }
+}
---
+++ GameJam2025_LostSignal/Assets/Scripts/UI/ButtonController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    [SerializeField] int buttonID;
+    public RoomManager roomManager;
+
+    public void OnButtonPressed()
+    {
+        GetComponent<AudioSource>().PlayDelayed(0.25f);
+        switch(buttonID)
+        {
+            case 1:
+                roomManager.SetRoomState(RoomManager.RoomState.LT);
+                break;
+            case 2:
+                roomManager.SetRoomState(RoomManager.RoomState.MT);
+                break;
+            case 3:
+                roomManager.SetRoomState(RoomManager.RoomState.RT);
+                break;
+
+            case 4:
+                roomManager.SetRoomState(RoomManager.RoomState.LM);
+                break;
+            case 5:
+                roomManager.SetRoomState(RoomManager.RoomState.MM);
+                break;
+            case 6:
+                roomManager.SetRoomState(RoomManager.RoomState.RM);
+                break;
+
+            case 7:
+                roomManager.SetRoomState(RoomManager.RoomState.LL);
+                break;
+            case 8:
+                roomManager.SetRoomState(RoomManager.RoomState.ML);
+                break;
+            case 9:
+                roomManager.SetRoomState(RoomManager.RoomState.RL);
+                break;
+
+            default:
+                roomManager.SetRoomState(RoomManager.RoomState.None);
+                Debug.Log("Hey Shitface, you forgot to write the Number for the Button. FUCK YOU!");
+                break;
+        }
+    }
+}
